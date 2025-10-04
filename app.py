573c88753f8cd4b8ee50045e528502d39b50dbba
Adhithya Jayaraman <adhithyajayaraman@Mac.box>
2025-10-04 17:21:57 +0200

    added mcp-server-functionality

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ llm = ChatGroq(model="llama-3.1-8b-instant", api_key=os.getenv("GROQ_API_KEY"))
 
 
 class mcp_server:
-    def __init__(self, mcp_server_name: str):
+    def __init__(self, mcp_server_config: str="mcp.json"):
         self.llm = llm
-        self.mcp_server_name = mcp_server_name
+        self.mcp_server_config = self.get_mcp_server_config(mcp_server_config)
 
-    def get_mcp_server_name(mcp_server_config: str="mcp.json"):
+
+    def get_mcp_server_config(mcp_server_config: str):
         with open(mcp_server_config, "r") as f:
             mcp_server_config = json.load(f)
         return mcp_server_config["mcpServers"]
